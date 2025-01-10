7a1c11212722773f0637a9e15149032398aebf65
Manju R <cskmnrpt@gmail.com>
2025-01-10 23:32:18 +0530

    add: examples for captureLogs

---
+++ tests/examples/captureLogs/test_stderr.py
@@ -0,0 +1,7 @@
+import sys
+
+
+def test_generate_stderr_logs():
+    """Test generating logs to stderr."""
+    for i in range(1, 51):  # Generating 50 lines of stderr logs
+        print(f"Error line {i} - This is a stderr message.", file=sys.stderr)
