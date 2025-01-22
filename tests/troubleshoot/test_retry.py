e2d503ee10b0b4ef48e6acc3a8bf6b772740cd8e
Manju R <cskmnrpt@gmail.com>
2025-01-22 19:06:07 +0530

    issue: retries are not reported (not-reproducible)
    The package 'pytest-rerunfailures' runs a failed/invalid test again 'n'
    number of times until it's passed. 'n' can be defined in the run
    command. Eg: pytest path/to/test.py --rerun 3

---
+++ tests/troubleshoot/test_retry.py
@@ -0,0 +1,14 @@
+import random
+
+import pytest
+from qase.pytest import qase
+
+
+@qase_id(1)
+def test_random_outcome():
+    outcome = random.choice(["pass", "invalid"])
+
+    if outcome == "pass":
+        assert True  # Test passes
+    elif outcome == "invalid":
+        raise SyntaxError("Test resulted in an invalid state as per random choice")
